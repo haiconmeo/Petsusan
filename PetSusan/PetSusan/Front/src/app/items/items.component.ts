f28e910dce9676832cafe821542a5aeb6afc15fd
bafousaixz <44966125+bafousaixz@users.noreply.github.com>
2019-11-29 17:03:28 +0700

    Detail Items

--- PetSusan/PetSusan/Front/src/app/items/items.component.ts
+++ PetSusan/PetSusan/Front/src/app/items/items.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ListItemsService } from '../_services/list-items.service';
 import { List } from '../_models/list-item.class';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 
 
 @Component({
@@ -13,14 +15,15 @@ export class ItemsComponent implements OnInit{
   public list : List[] =[];
 
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     public listItem : ListItemsService
   ) { }
 
   ngOnInit() {
     this.showItem();
-    this.listItem.getAllItems()
+    this.listItem.getAllItems();
   }
-
   
   showItem(){
      this.listItem.getAllItems().subscribe((data) => 
